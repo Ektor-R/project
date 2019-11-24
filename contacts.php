7c9f67281452dd2f58bf75603e7064d93d7f8928
Ektor-R <55707708+Ektor-R@users.noreply.github.com>
2019-11-25 01:04:03 +0200

    Style

--- contacts.php
+++ contacts.php
@@ -23,7 +23,7 @@
         if(isset($_GET['contactsearch']) && $_GET['contactsearch'] !== ''){
             $contactsearch = $con->real_escape_string($_GET['contactsearch']);
 
-            $result = $con->query("SELECT name, phone FROM contacts WHERE name LIKE '$contactsearch%' OR phone = '$contactsearch'");
+            $result = $con->query("SELECT * FROM contacts WHERE name LIKE '$contactsearch%' OR phone = '$contactsearch'");
 
         }else{
             $ok=false;
@@ -36,83 +36,124 @@
 
 <!DOCTYPE>
 <html>
-<head>
-    <title>Contacts</title>
-</head>
+    <head>
+        <title>Contacts</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
 <body>
-
-
-<!-- search bar -->
-
-<form method="GET" action="">
-    <input type="search" name="contactsearch" placeholder="Search" value="<?php echo htmlspecialchars($contactsearch); ?>">
-    <input type="submit" name="search" value="Search">
-</form>
-
-
-<!-- contacts table -->
-
-<table border="1">
-    <caption>Contacts</caption>
-    <tr>
-        <th>Name</th>
-        <th>Number</th>
-        <th>Action</th>
-    </tr>
-    <?php
-        foreach($contacts as $row){
-            echo "<tr>
-                <td>" . htmlspecialchars($row['name']) . "</td>
-                <td>" . htmlspecialchars($row['phone']) . "</td>
-                <td><a href=edit.php?id=". $row['id'] .">edit</a> <a href=delete.php?id=". $row['id'] .">delete</a></td>
-             </tr> ";
-        }
-    ?>
-</table><br>
-
-
-<!--search output result-->
-
-<?php
-if($ok) {
-    if($result->num_rows) {
-        foreach ($result as $row) {
-            echo htmlspecialchars($row['name']) . ": " . htmlspecialchars($row['phone']) . "<br>";
-        }
-    }else{
-        echo htmlspecialchars($contactsearch) . " is not in your contacts.";
-    }
-}
-
-$con->close();
-?>
-
-
-<!--new contact-->
-
-<div>
-    <div>
-        <h3>New Contact</h3>
+<div class="container-fluid">
+    <!-- search bar -->
+    <div class="row d-flex justify-content-center">
+        <form class="form-inline d-flex justify-content-center" method="GET" action="">
+            <input class="form-control" type="search" name="contactsearch" placeholder="Search" value="<?php echo htmlspecialchars($contactsearch); ?>">
+            <input type="submit" name="search" value="Search" class="btn btn-secondary">
+        </form>
     </div>
+     <!-- contacts table -->
+    <div class="row d-flex justify-content-center">
+        <div>
+           <table class="table table-bordered table-sm">
+                <caption>
+                    Contacts
+                </caption>
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Name</th>
+                        <th>Number</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($contacts as $row){
+                            echo "<tr>
+                                <td>" . htmlspecialchars($row['name']) . "</td>
+                                <td>" . htmlspecialchars($row['phone']) . "</td>
+                                <td><a class='btn btn-secondary btn-sm' role='button' href=edit.php?id=". $row['id'] .">edit</a> 
+                                    <a class='btn btn-secondary btn-sm' role='button' href=delete.php?id=". $row['id'] .">delete</a></td>
+                            </tr> ";
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!--search output result-->
+    <div class="row d-flex justify-content-center">
+            <?php
+            if($ok) {
+                if($result->num_rows) {
+            ?>
+            <div>
+                <table class="table table-borderless table-sm table-responsive">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Name</th>
+                            <th>Number</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                <?php                    
+                    foreach ($result as $row) {
+                        echo "
+                            <tr>
+                                <td>" . htmlspecialchars($row['name']) . "</td>
+                                <td>" . htmlspecialchars($row['phone']) . "</td>
+                                <td><a class='btn btn-secondary btn-sm' role='button' href=edit.php?id=". $row['id'] .">edit</a> 
+                                    <a class='btn btn-secondary btn-sm' role='button' href=delete.php?id=". $row['id'] .">delete</a></td>
+                            </tr>";
+                    }
+                ?>  </tbody>
+                </table>
+            </div>
+            <?php
+                }else{
+                    echo htmlspecialchars($contactsearch) . " is not in your contacts.";
+                }
+            }
 
+            $con->close();
+            ?>
+    </div>
+    
     <hr>
 
-    <form method="POST" action="">
-        <div>
-            <label>Name</label>
-            <input type="text" name="newname" placeholder="e.g. Nikos" required>
-        </div>
-        <div>
-            <label>Phone</label>
-            <input type="text" name="newphone" placeholder="e.g. 6983574327" required>
-        </div>
-        <div>
-            <input type="submit" name="add" value="Add">
-            <input type="reset" value="Reset">
-        </div>
-    </form>
-</div>
+    <!--new contact-->
 
+    <div class="row d-flex justify-content-center">
+        <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#newC" aria-expanded="false" aria-controls="collapseExample">
+            <h5>New Contact</h5>
+        </button>
+    </div>
+    <div class="collapse" id="newC">
+        <div class="row d-flex justify-content-center">
+        <form method="POST" action="">
+            <div class="form-group">
+                <label>Name</label>
+                <input class="form-control" type="text" name="newname" placeholder="e.g. Nikos" required>
+            </div>
+            <div class="form-group"> 
+                <label>Phone</label>
+                <input class="form-control" type="text" name="newphone" placeholder="e.g. 6983574327" required>
+            </div>
+            <div>
+                <input class="btn btn-secondary" type="submit" name="add" value="Add">
+                <input class="btn btn-secondary" type="reset" value="Reset">
+            </div>
+        </form>
+        </div>
+    </div>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
 
--- edit.php
+++ edit.php
@@ -31,32 +31,38 @@
 <html>
 <head>
     <title>Edit</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <h3>Edit Contact</h3>
+    </div>
 
-<div>
-    <h3>Edit Contact</h3>
+    <div class="row d-flex justify-content-center">
+        <form method="GET" action="">
+            <?php foreach($contact as $row){?>
+            <div class="form-group">
+                <label>Name</label>
+                <input class="form-control" type="text" name="editname" value="<?php echo $row['name']; ?>" required>
+            </div>
+            <div class="form-group">
+                <label>Phone</label>
+                <input  class="form-control" type="text" name="editphone" value="<?php echo $row['phone']; ?>" required>
+            </div>
+            <?php } ?>
+            <div>
+                <input class="btn btn-secondary" type="submit" name="edit" value="Edit">
+                <input class="btn btn-secondary" type="reset" value="Reset">
+            </div>
+            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+        </form>
+    </div>
 </div>
-
-<div>
-    <form method="GET" action="">
-        <?php foreach($contact as $row){?>
-        <div>
-            <label>Name</label>
-            <input type="text" name="editname" value="<?php echo $row['name']; ?>" required>
-        </div>
-        <div>
-            <label>Phone</label>
-            <input type="text" name="editphone" value="<?php echo $row['phone']; ?>" required>
-        </div>
-        <?php } ?>
-        <div>
-            <input type="submit" name="edit" value="Edit">
-            <input type="reset" value="Reset">
-        </div>
-        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-    </form>
-</div>
-
 </body>
 </html>
